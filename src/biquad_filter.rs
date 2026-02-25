850a2cb019b1d5b69501ee6a7616f268a7379b36
Wasaka <mendeztrinidadmiguel@gmail.com>
2026-02-25 09:33:33 +0100

    Add biquad internal structures

--- src/biquad_filter.rs
+++ src/biquad_filter.rs
@@ -15,7 +15,28 @@
 // A biquad filter that provides multiple filter types 
 #[derive(Clone, Debug, Default)]
 pub struct BiquadFilter{
+    coefficients: BiquadCoefficients,
+    samples: SampleStorage
 }
 
+// Coefficients used to process the filter output
+#[derive(Clone, Debug, Default)]
+struct BiquadCoefficients{
+    b0: f32,
+    b1: f32,
+    b2: f32,
+    a1: f32,
+    a2: f32
+}
+
+// Samples that need to be recalled during filtering process
+#[derive(Clone, Debug, Default)]
+struct SampleStorage{
+    x1: f32,
+    x2: f32,
+    y0: f32,
+    y1: f32,
+    y2: f32
+}
 impl BiquadFilter {
 }
