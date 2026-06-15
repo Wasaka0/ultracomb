c738f057881bde74b651d6546ba592e6e5fbae06
Wasaka <mendeztrinidadmiguel@gmail.com>
2026-06-15 10:32:59 +0200

    Tweak UI element spacing

--- src/editor.rs
+++ src/editor.rs
@@ -8,7 +8,7 @@ use crate::custom_param_slider::CustomParamSlider;
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::new(|| (400, 250))
+    ViziaState::new(|| (400, 260))
 }
 
 pub(crate) fn create(
@@ -30,35 +30,38 @@ pub(crate) fn create(
                 .font_weight(FontWeightKeyword::Regular)
                 .font_size(15.0)
                 .top(Stretch(1.0));
-        }).height(Stretch(0.2))
-        .bottom(Pixels(5.0))
+        })
+        .height(Stretch(0.2))
+        .padding(Pixels(5.0))
         .class("background-banner");
         // Sliders
         HStack::new(cx, |cx|{
             VStack::new(cx, |cx| {
                 Label::new(cx, "Flanger");
-                CustomParamSlider::new(cx,&params.flanging);
+                CustomParamSlider::new(cx,&params.flanging).width(Stretch(1.0));
                 Label::new(cx, "Dry Delay (Chaos)");
-                CustomParamSlider::new(cx, &params.chaos);
+                CustomParamSlider::new(cx, &params.chaos).width(Stretch(1.0));
                 Label::new(cx, "Phaser");
-                CustomParamSlider::new(cx, &params.phasing);
+                CustomParamSlider::new(cx, &params.phasing).width(Stretch(1.0));
                 Label::new(cx, "Speed");
-                CustomParamSlider::new(cx, &params.speed);
+                CustomParamSlider::new(cx, &params.speed).width(Stretch(1.0));
             })
-            .left(Stretch(1.0))
-            .right(Stretch(1.0));
+            .width(Stretch(0.5))
+            .padding(Pixels(5.0));
             VStack::new(cx, |cx| {
                 Label::new(cx, "Low-Cut");
-                CustomParamSlider::new(cx, &params.low);
+                CustomParamSlider::new(cx, &params.low).width(Stretch(1.0));
                 Label::new(cx, "High-Cut");
-                CustomParamSlider::new(cx, &params.high);
+                CustomParamSlider::new(cx, &params.high).width(Stretch(1.0));
                 Label::new(cx, "Dry/Wet");
-                CustomParamSlider::new(cx, &params.strength);
+                CustomParamSlider::new(cx, &params.strength).width(Stretch(1.0));
                 Label::new(cx, "Multiplier");
-                CustomParamSlider::new(cx, &params.multiplier);
+                CustomParamSlider::new(cx, &params.multiplier).width(Stretch(1.0));
             })
-            .left(Stretch(1.0))
-            .right(Stretch(1.0));
-        }).class("background-main");
+            .width(Stretch(0.5))
+            .padding(Pixels(7.0));  
+        })
+        .class("background-main")
+        .padding(Pixels(2.0));
     })
 }
